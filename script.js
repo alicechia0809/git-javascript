dc2ef7ec0a239dde007da852c191bfd2434e0974
Chih chin Chia <alicechia@Chihs-MacBook-Pro.local>
2020-01-09 18:41:12 -0500

    finished the calculation

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 var name = 'Alice chia'
-console.log('Hello, my name is name!')
+console.log(`Hello, my name is ${name}!`)
 var number = 8
-concole.log(number)
+var step1 = number * 2
+var step2 = step1 * 5
+var step3 = step2 / number
+var step4 = step3 - 7
+console.log(step4)
